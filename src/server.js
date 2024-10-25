2e0068e031c52fd2f230fc6571d630a8a11c0e3f
Tan Pham <tan.phamx@titancorpvn.com>
2024-10-25 13:57:22 +0700

    Video #57: Thuc hien ket noi den MongoDB

--- src/server.js
+++ src/server.js
@@ -19,14 +19,8 @@ configViewEngine(app);
 //app.use('/v1', webRoutes);  URL:http://localhost:3000/v1/.... 
 app.use('/', webRoutes);
 
-// A simple SELECT query
-// connection.query(
-//     'select * from Users u',
-//     function (err, results, fields) {
-//         console.log(">>>> check result: ", results); // results contains rows returned by server
-//         console.log(">>>> check fields: ", fields); // fields contains extra meta data about results, if available
-//     }
-// );
+// test connection MongoDB
+connection();
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
