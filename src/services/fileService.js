700346b1174108f62b471e62e2cea71dc1936a26
TanPham <xuantan.pham2808@gmail.com>
2024-10-26 15:52:40 +0700

    Video #85.1: Upload single file & che bien lai ten file

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -1,13 +1,25 @@
+const path = require("path");
 
 const uploadSingleFile = async (fileObject) => {
     // Đường dẫn tuyệt đối
-    let uploadPath = __dirname + '/aa/' + fileObject.name;
+    //let uploadPath = __dirname + fileObject.name;
+
+    // Lưu ảnh vào public/images/upload
+    let uploadPath = path.resolve(__dirname, "../public/images/upload")
+
+    // chế biến lại tên ảnh
+    let extName = path.extname(fileObject.name); // lây tên đuôi file
+    let baseName = path.basename(fileObject.name, extName)
+
+    // create final path: eg: /upload/your-image.png
+    let finalName = `${baseName}-${Date.now()}${extName}`  // VD: abc-26102024030011.png
+    let finalPath = `${uploadPath}/${finalName}`;
 
     try {
-        await fileObject.mv(uploadPath);
+        await fileObject.mv(finalPath);
         return {
             status: 'success',
-            path: uploadPath,
+            path: finalName,
             error: null
         }
 
