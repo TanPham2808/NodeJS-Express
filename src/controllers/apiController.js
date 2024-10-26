3dce5202990655fb1cc113ba7f986cd655d0628a
TanPham <xuantan.pham2808@gmail.com>
2024-10-26 13:16:13 +0700

    Video #74: Build API create post User

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -9,6 +9,27 @@ const getUserAPI = async (req, res) => {
     })
 }
 
+const postUserAPI = async (req, res) => {
+    try {
+        let user = await User.create({
+            email: req.body.email,
+            name: req.body.name,
+            city: req.body.city
+        })
+
+        return res.status(200).json({
+            errorCode: 0,
+            data: user
+        })
+    } catch (err) {
+        return res.json({
+            errorCode: 1,
+            data: "Error create User"
+        })
+    }
+
+}
+
 module.exports = {
-    getUserAPI
+    getUserAPI, postUserAPI
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getUserAPI } = require('../controllers/apiController')
+const { getUserAPI, postUserAPI } = require('../controllers/apiController')
 
 const routerAPI = express.Router();
 
 routerAPI.get('/users', getUserAPI);
+routerAPI.post('/users', postUserAPI);
 
 module.exports = routerAPI
